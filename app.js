ea7c4a9504938f2efd0454ab98e7be31c9935070
andika.pelealu90 <andika.pelealu90@gmail.com>
2020-11-01 02:37:25 +0700

    adding blueprints

---
+++ app.js
@@ -0,0 +1,10 @@
+require('dotenv/types').config();
+const express = require('express');
+const app = express();
+const  router  = require('./routes/index')
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+app.use(router)
+const PORT = Number(process.env.PORT) || 3000;
+app.listen(PORT,console.log('Server running on port',PORT))
